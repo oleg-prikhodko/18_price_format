784fefc32709e0683f8b7122101744cbb83c5363
Oleg <super.oleg.3000@yandex.ru>
2018-10-08 17:39:32 +0300

    Fix tests

--- tests.py
+++ tests.py
@@ -6,6 +6,7 @@ from format_price import format_price
 
 class TestPriceFormatter(unittest.TestCase):
     def test_format_int_price(self):
+        self.assertEqual(format_price(0), "0")
         self.assertEqual(format_price(1), "1")
         self.assertEqual(format_price(123), "123")
         self.assertEqual(format_price(2018), "2 018")
@@ -14,6 +15,7 @@ class TestPriceFormatter(unittest.TestCase):
         self.assertEqual(format_price(-1000), "-1 000")
 
     def test_format_float_price(self):
+        self.assertEqual(format_price(0.0), "0")
         self.assertEqual(format_price(1000.0000), "1 000")
         self.assertEqual(format_price(100000.325235), "100 000.325235")
         self.assertEqual(format_price(-77.234), "-77.234")
@@ -29,11 +31,11 @@ class TestPriceFormatter(unittest.TestCase):
         self.assertIsNone(format_price("123.6ggg7"))
         self.assertIsNone(format_price("ABC"))
         self.assertIsNone(format_price("123.000.555"))
-        self.assertIsNone(format_price("001.555"))
 
     def test_format_price_other(self):
         self.assertIsNone(format_price(None))
-        self.assertIsNone(format_price(object))
+        self.assertIsNone(format_price(object()))
+
 
 if __name__ == "__main__":
     unittest.main()
