9f93597b9beac633e5188081c3ebf27b425b1c69
Oleg <super.oleg.3000@yandex.ru>
2018-10-10 11:51:26 +0300

    Update validation

--- format_price.py
+++ format_price.py
@@ -1,11 +1,30 @@
 import argparse
+import math
+import re
 import sys
+from numbers import Number
+
+
+def is_valid_price(price):
+    if (
+        not isinstance(price, Number) and not isinstance(price, str)
+    ) or isinstance(price, bool):
+        return False
+    elif isinstance(price, str) and re.search(r"[^\d.+-]", price) is not None:
+        return False
+    elif isinstance(price, Number) and not math.isfinite(price):
+        return False
+    else:
+        return True
 
 
 def format_price(price):
+    if not is_valid_price(price):
+        return None
+
     try:
         float_price = float(price)
-    except (ValueError, TypeError):
+    except ValueError:
         return None
 
     if float_price == 0:
